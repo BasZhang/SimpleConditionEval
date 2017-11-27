6a0562d304cd5880bc0f94945f5e555c7f581142
BasZhang <BasZhang@users.noreply.github.com>
2017-11-27 15:35:26 +0800

    Update Tokens.java

--- src/main/java/zorg/simpleCondition/Tokens.java
+++ src/main/java/zorg/simpleCondition/Tokens.java
@@ -2,26 +2,31 @@ package zorg.simpleCondition;
 
 public class Tokens {
 
-    public static final Token AND = new Operator("&", 4);
-
-    public static final Token OR  = new Operator("|", 5);
-
-    public static final Token NOT = new Operator("！", 3);
-
-    public static final Token CONDITIONAL = new Operator("?", 6);
-
-    public static final Token OPTION = new Operator(":", 7);
-
-    public static final Token OPEN_PAREN = new Operator("(", 2);
-
-    public static final Token CLOSE_PAREN = new Operator(")", 2);
-
     public static final Token REF = new Operator("@", 1);
+    public static final Token NOT = new Operator("!", 2);
+    public static final Token AND = new Operator("&", 3);
+    public static final Token OR = new Operator("|", 4);
+    public static final Token CONDITIONAL = new Operator("?", 5);
+    public static final Token OPTION = new Operator(":", 5);
+
+    public static final Token OPEN_PAREN = new Paren("(");
+    public static final Token CLOSE_PAREN = new Paren(")");
 
     public static Token IDENTIFIER(String identifier) {
         return new Identifier(identifier);
     }
 
+    public static Token VALUE(boolean v) {
+        Identifier val = new Identifier(Boolean.toString(v));
+        val.ref = v ? ICondition.TRUE : ICondition.FALSE;
+        return val;
+    }
+
+    public static Token BINDED_REF(ICondition lazyCondition) {
+        Identifier ref = new Identifier(null);
+        ref.ref = lazyCondition;
+        return ref;
+    }
 
 }
 
@@ -40,9 +45,7 @@ class Operator extends Token {
 
     @Override
     public String toString() {
-        return "Operator{" +
-                "op='" + op + '\'' +
-                '}';
+        return "Operator{" + "op='" + op + '\'' + '}';
     }
 }
 
@@ -56,9 +59,22 @@ class Identifier extends Token {
 
     @Override
     public String toString() {
-        return "Identifier{" +
-                "identifier='" + identifier + '\'' +
-                ", ref=" + ref +
-                '}';
+        return "Identifier{" + "identifier='" + identifier + '\'' + ", ref=" + ((ref == null) ? null : "ref" + ref.hashCode()) + '}';
     }
+
+}
+
+class Paren extends Token {
+    final String op;
+
+    public Paren(String op) {
+        super();
+        this.op = op;
+    }
+
+    @Override
+    public String toString() {
+        return "Paren{op='" + op + "'}";
+    }
+
 }
